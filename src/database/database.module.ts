915d4f28b46d1ee32d5304aa0ca45824085c2b7c
Burak Can Karasel <99825584+burakkarasel@users.noreply.github.com>
2023-09-15 19:16:25 +0300

    Added nestjs/config dependency, database module

---
+++ src/database/database.module.ts
@@ -0,0 +1,29 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { EntityClassOrSchema } from "@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type";
+
+@Module({
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.getOrThrow("POSTGRES_HOST"),
+        port: +configService.getOrThrow("POSTGRES_PORT"),
+        username: configService.getOrThrow("POSTGRES_USER"),
+        password: configService.getOrThrow("POSTGRES_PASSWORD"),
+        database: configService.getOrThrow("POSTGRES_DB"),
+        synchronize: configService.getOrThrow("POSTGRES_SYNC"),
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {
+  static forFeature(entities: EntityClassOrSchema[]) {
+    return TypeOrmModule.forFeature(entities);
+  }
+}
